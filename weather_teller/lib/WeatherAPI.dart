cf786338be3c64063c517ef814c07ec3ee8a4466
rodrigo mendes <rodrigocm1999@hotmail.com>
2022-01-10 19:41:33 +0000

    wssih sh shw

---
+++ weather_teller/lib/WeatherAPI.dart
@@ -0,0 +1,20 @@
+
+class WeatherInfoDay{
+
+
+
+}
+
+class WeatherAPI{
+
+
+
+  //Call 16 day / daily forecast data
+  //api.openweathermap.org/data/2.5/forecast/daily?q={city name}&cnt={cnt}&appid={API key}
+
+
+  //&appid=f748cf33755afb0c35061a61f1d8b9d7
+
+
+
+}
